30f80d82bd77790c5e9e543ab31d2b7af28b80e9
Julio Rosaldo <julio.rosaldo@live.com.mx>
2020-10-05 09:11:57 -0500

    Second test

--- src/main/java/com/example/mynavigationdrawer2/MainActivity.java
+++ src/main/java/com/example/mynavigationdrawer2/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        int doge = 99;
         return true;
     }
 
